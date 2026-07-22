a7cf70b63b0a1c2395dbb5859f5a49f5a7b17636
mrizkyanargyasofyan-eng <m.rizkyanargyasofyan@gmail.com>
2026-07-22 11:59:34 +0700

    Fix typescript error env supabase

--- debug.ts
+++ debug.ts
@@ -2,7 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import * as dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+// Tambahin tanda seru (!) di akhir variabel process.env
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
 async function main() {
   const { data, error } = await supabase.from('shift_inventory').select('*').limit(1);
